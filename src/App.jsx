3af3917e20d036a3681da276617cee7521e1d560
abdullahamir1233 <bscs21042@itu.edu.pk>
2024-10-06 18:53:23 +0500

    Homepage Banner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Header from "./components/header"
 import Footer from "./components/footer"
-
+import HomepageBanner from "./components/HomepageBanner"
 export default function App() {
   return (
     <div>
       <Header />
+<HomepageBanner />
     <Footer />
         </div>
   )
